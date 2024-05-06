4251ef5caedf80232f96f67bfffd54d84e254121
Mark <m.web.jan@gmail.com>
2024-05-07 01:24:43 +0300

    feat: build slider section

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
-import './style.scss'
+import "./style.scss";
+
+function animateSlider() {
+  const slider = document.querySelector("#sliderCards") as HTMLElement;
+  const nextButton = document.querySelector("#sliderBtnRight") as HTMLElement;
+  const prevButton = document.querySelector("#sliderBtnLeft") as HTMLElement;
+  const cardsCount = slider.getElementsByTagName("img").length;
+
+  let currentTranslateX = 0;
+  let cardWidth = 390;
+
+  prevButton.addEventListener("click", () => {
+    currentTranslateX += currentTranslateX === 0 ? 0 : cardWidth;
+    slider.style.transform = `translateX(${currentTranslateX}px)`;
+  });
+
+  nextButton.addEventListener("click", () => {
+    console.log(currentTranslateX);
+
+    currentTranslateX -=
+      -cardsCount + 3 !== currentTranslateX / cardWidth ? cardWidth : 0;
+    slider.style.transform = `translateX(${currentTranslateX}px)`;
+  });
+}
+
+animateSlider();
