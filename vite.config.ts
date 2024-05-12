64544f78fc38c7c59129b85c19ff4d093cf8c62e
Mark <m.web.jan@gmail.com>
2024-05-12 15:06:27 +0300

    feat: build contact page

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig ({
       input: {
         main: resolve(__dirname, "index.html"),
         exhibits: resolve(__dirname, "exhibits/index.html"),
+        contacts: resolve(__dirname, "contacts/index.html"),
       },
     },
   },
