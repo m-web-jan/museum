a737a731c231b1d92f4cf4dbf1f326ad67e4565a
Mark <m.web.jan@gmail.com>
2024-05-26 18:05:57 +0300

    feat: add ymap

---
+++ contacts/map.ts
@@ -0,0 +1,17 @@
+function init() {
+  let map = new ymaps.Map('map', {
+    center: [52.07996756477022,23.65463858036848],
+    zoom: 16
+  });
+
+  map.controls.remove('geolocationControl');
+  map.controls.remove('searchControl');
+  map.controls.remove('trafficControl');
+  map.controls.remove('typeSelector');
+  map.controls.remove('fullscreenControl');
+  map.controls.remove('zoomControl');
+  map.controls.remove('rulerControl');
+  map.behaviors.disable(['scrollZoom']);
+}
+
+ymaps.ready(init);
